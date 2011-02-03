7e71090f07ad4dcf33c2df1ebaaa72fad81751a5
Libor Zoubek <lzoubek@jezzovo.net>
2011-02-03 19:33:07 +0100

    finished implementing generating links to services via cmdline args

--- xbmc-export.py
+++ xbmc-export.py
@@ -152,17 +152,6 @@ class RPCClient(object):
 			update_fields(seasons,['seasons'])
 			data.append({'label':show['label'],'count': len(seasons['seasons']),'rating':'%.2f'%show['rating'],'year':show['year'],'genre':show['genre']})
 		return data
-def filter_for_link(string):
-	return string.replace('\"','')
-def csfd_link(moviename,title):
-	moviename = filter_for_link(moviename)
-	return  """<a target="_blank" href="%shttp://csfd.cz/hledani-filmu-hercu-reziseru-ve-filmove-databazi/?search=%s">%s</a>"""%(ANONYM_TO,moviename,title)
-
-def imdb_link(moviename,title):
-	moviename = filter_for_link(moviename)
-	return  """<a target="_blank" href="%shttp://www.imdb.com/find?s=all&q=%s">%s</a>"""%(ANONYM_TO,moviename,title)
-def link(moviename):
-	return '%s %s'%(csfd_link(moviename,'CSFD'),imdb_link(moviename,'IMDB'))
 
 def update_fields(item,fields):
 	for field in fields:
@@ -170,48 +159,76 @@ def update_fields(item,fields):
 			item[field]=''
 	return item
 
-def parse_movies(data,sort=True):
-	movies = []
-	if sort:
-		data['movies'] = sorted(data['movies'],key=itemgetter('label'))
-	for movie in data['movies']:
-		movie = update_fields(movie,['year','genre','file','label','rating','duration'])
-		movie['rating'] = '%.2f' % movie['rating']
-		movie['duration'] = '%im' % (int(movie['duration'])/60)
-		m = MOVIE % (len(movies)+1,movie['label'],link(movie['label']),movie['year'],movie['genre'],movie['rating'],movie['duration'],os.path.basename(movie['file']))
-		movies.append(m)
-	return movies
-def parse_series(data):
-	series = []
-	data = sorted(data,key=itemgetter('label'))
-	for show in data:
-		s = TVSHOW % (len(series)+1,show['label'],link(show['label']),show['count'],show['year'],show['genre'],show['rating'])
-		series.append(s)
-	return series
+class Generator(object):
+	def __init__(self,anonymize=False,names=[],uris=[]):
+		self.anonymize = ''
+		if anonymize:
+			self.anonymize = 'http://anonym.to?'
+		self.services=[]
+		for i in range(0,len(names)):
+			self.services.append({'name':names[i],'uri':uris[i]})
 
-ANONYM_TO='http://anonym.to/?'
+	def filter_for_link(self,string):
+		return string.replace('\"','')
+	def _link(self,moviename,title,uri):
+		moviename = self.filter_for_link(moviename)
+		return  """<a target="_blank" href="%s%s%s">%s</a>"""%(self.anonymize,uri,moviename,title)
+	def _get_links(self,moviename):
+		links = []
+		for service in self.services:
+			links.append(self._link(moviename,service['name'],service['uri']))
+			links.append(' ')
+		return ''.join(links) 
+
+	def parse_movies(self,data,sort=True):
+		movies = []
+		if sort:
+			data['movies'] = sorted(data['movies'],key=itemgetter('label'))
+		for movie in data['movies']:
+			movie = update_fields(movie,['year','genre','file','label','rating','duration'])
+			movie['rating'] = '%.2f' % movie['rating']
+			movie['duration'] = '%i' % (int(movie['duration'])/60)
+			m = MOVIE % (len(movies)+1,movie['label'],self._get_links(movie['label']),movie['year'],movie['genre'],movie['rating'],movie['duration'],os.path.basename(movie['file']))
+			movies.append(m)
+		return movies
+
+	def parse_series(self,data):
+		series = []
+		data = sorted(data,key=itemgetter('label'))
+		for show in data:
+			s = TVSHOW % (len(series)+1,show['label'],self._get_links(show['label']),show['count'],show['year'],show['genre'],show['rating'])
+			series.append(s)
+		return series
+
 usage="""%prog [options]"""
 parser = optparse.OptionParser(usage=usage)
 parser.add_option('-o','--output',dest='output',default='.',metavar='DIR',help='write output to DIR')
 parser.add_option('-u','--username',dest='username',default=None,help='authentication user')
 parser.add_option('-p','--password',dest='password',default=None,help='authentication password')
 parser.add_option('-s','--source',dest='source',default='http://localhost:8080/jsonrpc',help='source URI of XBMC [default %default]')
-parser.add_option('-l','--links',dest='links',default='http://imdb.com,http://themoviedb.org/search?search=',help='comma-separated list of URIs of services to generate search links for, DEFAULT=%default')
-parser.add_option('-n','--names',dest='names',default='IMDB,TheMovieDB',help='coma-separated list of --links names, DEFAULT=%default')
-parser.add_option('-a','--anonymize',dest='anonymize',action='store_true',default=False,help='enable anonymizing links via http://anonym.to')
+parser.add_option('-l','--links',dest='links',default='http://imdb.com/find?s=all&q=,http://themoviedb.org/search?search=',help='comma-separated list of URIs of services to generate search links for, DEFAULT=%default')
+parser.add_option('-n','--names',dest='names',default='IMDB,MovieDB',help='coma-separated list of --links names, DEFAULT=%default')
+parser.add_option('-a','--anonymize',dest='anonymize',action='store_true',default=False,help='enable anonymous links via http://anonym.to')
 (options,args) = parser.parse_args()
 if not os.path.exists(options.output):
-	print 'Output dir %s does not exist!'%options.output
+	print 'Output dir %s does not exist! '%options.output
 	sys.exit(1)
-if not options.anonymize:
-	ANONYM_TO=''
 try:
 	links =  options.links.split(',')
-	print links
 except:
 	print 'Unable to parse --links argument'
 	traceback.print_exc()
 	sys.exit(1)
+try:
+	names = options.names.split(',')
+except:
+	print 'Unable to parse --names argument'
+	traceback.print_exc()
+	sys.exit(1)
+if not len(names) == len(links):
+	print '--links and --names must be comma-separated lists having same sizes'
+	sys.exit(1)
+
 print 'Getting movies'
 reader = RPCClient(options.source,options.username,options.password)
 movies = reader.get_movies()
@@ -222,12 +239,13 @@ recent_movies = reader.get_recently_added_movies()
 print 'Getting shows'
 tv_shows = reader.get_tv_shows()
 #tv_shows = {}
+generator = Generator(options.anonymize,names,links)
 try:
 	print 'Writing output to %s' % options.output
 	f = open(options.output+'/index.html','w')
-	movies = parse_movies(movies)
-	recent_movies = parse_movies(recent_movies,sort=False)
-	tv_shows = parse_series(tv_shows)
+	movies = generator.parse_movies(movies)
+	recent_movies = generator.parse_movies(recent_movies,sort=False)
+	tv_shows = generator.parse_series(tv_shows)
 	html = HTML % (datetime.datetime.now().strftime('%d. %m. %y at %H:%M:%S'),''.join(movies),''.join(recent_movies),''.join(tv_shows))
 	f.write(html.encode('ascii','ignore'))
 	print 'Done'
@@ -242,4 +260,5 @@ try:
 	print 'Done'
 except:
 	traceback.print_exc()
+	sys.exit(1)
 f.close()
